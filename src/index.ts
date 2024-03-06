8a40fcabdd03b0c69a3535deab96ce5a310c4951
user530 <m.timarsuev@gmail.com>
2024-03-06 22:29:46 +0300

    Initial Express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world! We will be polling!')
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// Load env variables
+dotenv.config();
+
+// Create App
+const app: Express = express();
+const port: number = typeof process.env.PORT === 'number' ? process.env.PORT : 5000;
+
+// Simple route handler
+app.get("/", (req: Request, res: Response) => {
+    res.send("Prepare to be polled, matey!!")
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Express server is up and running at port ${port}...`);
+});
